95861b8bf229766ebd1364829afde85d69e42f2b
Julian Kahlert <julian.kahlert@avionic-design.de>
2022-02-03 14:37:35 +0100

    build.sh: Support use as submodule
    Signed-off-by: Julian Kahlert <julian.kahlert@avionic-design.de>

--- build.sh
+++ build.sh
@@ -38,7 +38,9 @@ check_cmd rm
 check_cmd mkdir
 check_cmd cd
 check_cmd tar
-check_cmd touch
+check_cmd realpath
+check_cmd dirname
+check_cmd readlink
 
 if test $FAIL = true
 then
@@ -47,4 +49,32 @@ then
 fi
 reset_fail
 
-make compile
+self_d=$(realpath $(dirname $(readlink -f ${0})))
+if test "$1" = '--init-submodule'
+then
+	prj_d="$2"
+	test "$prj_d" = '' && prj_d="$PWD"
+	prj_d=$(realpath ${prj_d})
+	test "$prj_d" = "$self_d" && exit 0
+	rel_d=$(realpath --relative-to="$self_d" "$prj_d")
+	if ! test -f "$prj_d/config.mk"
+	then
+		echo  "ARDUINO_PFM := arduino:avr"       >  "$prj_d/config.mk"
+		echo  "ARDUINO_BRD := nano"              >> "$prj_d/config.mk"
+		echo  "ARDUINO_CPU := atmega328"         >> "$prj_d/config.mk"
+		echo  "ARDUINO_OUT := build"             >> "$prj_d/config.mk"
+		echo  "ARDUINO_SRC := ${rel_d}/src"      >> "$prj_d/config.mk"
+		echo  "ARDUINO_HOME := ${self_d}"        >> "$prj_d/config.mk"
+		echo  "ARDUINO_SKETCH := main"           >> "$prj_d/config.mk"
+		echo  "ARDUINO_CLI_VER := 0.20.2"        >> "$prj_d/config.mk"
+		echo  "ARDUINO_FILE_BIN := bin"          >> "$prj_d/config.mk"
+		echo  "ARDUINO_FILE_ETC := ${rel_d}/etc" >> "$prj_d/config.mk"
+		echo  "ARDUINO_FILE_RES := .res"         >> "$prj_d/config.mk"
+	fi
+	cd "$self_d"
+	rm -f config.mk
+	ln -s "$prj_d/config.mk" config.mk
+else
+	cd "$self_d"
+	make compile
+fi
